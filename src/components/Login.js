aa07b2c0055cff40fd33de9dbbb0f33a3c61f8f3
Matthew Long <MattLong87@gmail.com>
2017-05-25 14:40:10 -0400

    form styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
+import '../css/form.css'
 import '../css/login.css'
 
 export class Login extends React.Component {
@@ -13,17 +14,17 @@ export class Login extends React.Component {
     render() {
         return (
             <div className='login'>
-            <form className='login-form' id='login-form' onSubmit={(e) => this.login(e)}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name='email' id='email' />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name='password' id='password' />
-                </div>
-                <button type='submit'>Log In</button>
-            </form>
+                <form className='login-form' id='login-form' onSubmit={(e) => this.login(e)}>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name='email' id='email' />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' id='password' />
+                    </div>
+                    <button type='submit'>Log In</button>
+                </form>
             </div>
         )
     }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
+import '../css/form.css'
 import '../css/signup.css'
 
 export class SignUp extends React.Component {
@@ -9,25 +10,27 @@ export class SignUp extends React.Component {
         this.props.dispatch(actions.signUpUser());
     }
     render() {
-        return (<form className='signup-form' id='signup-form' onSubmit={(e) => this.signUp(e)}>
-            <div>
-                <label htmlFor="firstName">First name</label>
-                <input placeholder='First Name' type="text" name='firstName' id='firstName' />
-            </div>
-            <div>
-                <label htmlFor="lastName">Last name</label>
-                <input type="text" name='lastName' id='lastName' placeholder='Last Name' />
-            </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="text" name='email' id='email' />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name='password' id='password' />
-            </div>
-            <button type='submit'>Sign Up</button>
-        </form>
+        return (<div className='signup'>
+            <form className='signup-form' id='signup-form' onSubmit={(e) => this.signUp(e)}>
+                <div>
+                    <label htmlFor="firstName">First name</label>
+                    <input placeholder='First Name' type="text" name='firstName' id='firstName' />
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last name</label>
+                    <input type="text" name='lastName' id='lastName' placeholder='Last Name' />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name='email' id='email' />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' id='password' />
+                </div>
+                <button type='submit'>Sign Up</button>
+            </form>
+        </div>
         )
     }
 }
