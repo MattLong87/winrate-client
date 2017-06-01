12a84b2501184d7fbd8abd424854072656a1911f
Matthew Long <MattLong87@gmail.com>
2017-06-01 11:09:25 -0400

    nav closes when clicking link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,12 +10,12 @@ export default function NavBar() {
             <ul>
                 <li className='top-li'><img src={hamburger} className='hamburger' onClick={(e) => toggleNavExpand(e)} /></li>
                 <section className='nav-links'>
-                    <li><Link to='/'>WinRate</Link></li>
-                    <li><Link to='/dashboard'>Dashboard</Link></li>
-                    <li><Link to='/all-sessions'>All Sessions</Link></li>
-                    <li><Link to='/add-session'>Add a Session</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
-                    <li><Link to='/sign-up'>Sign Up</Link></li>
+                    <li><Link to='/' onClick={(e) => linkToggleNavExpand(e)}>WinRate</Link></li>
+                    <li><Link to='/dashboard' onClick={(e) => linkToggleNavExpand(e)}>Dashboard</Link></li>
+                    <li><Link to='/all-sessions' onClick={(e) => linkToggleNavExpand(e)}>All Sessions</Link></li>
+                    <li><Link to='/add-session' onClick={(e) => linkToggleNavExpand(e)}>Add a Session</Link></li>
+                    <li><Link to='/login' onClick={(e) => linkToggleNavExpand(e)}>Login</Link></li>
+                    <li><Link to='/sign-up' onClick={(e) => linkToggleNavExpand(e)}>Sign Up</Link></li>
                 </section>
             </ul>
         </nav>
@@ -24,4 +24,8 @@ export default function NavBar() {
 
 function toggleNavExpand(e) {
     e.target.parentElement.parentElement.parentElement.classList.toggle('nav-expanded');
+}
+
+function linkToggleNavExpand(e) {
+    e.target.parentElement.parentElement.parentElement.parentElement.classList.toggle('nav-expanded');
 }
