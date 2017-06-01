b9ae6e23038b94aa563e9109476ff2cd36f5ab54
Matthew Long <MattLong87@gmail.com>
2017-06-01 11:07:17 -0400

    made navbar mobile-friendly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import hamburger from '../../img/hamburger.png';
 import '../css/navbar.css'
 
 export default function NavBar() {
 
     return (
         <nav>
-        <ul>
-            <li><Link to='/'>WinRate</Link></li>
-            <li><Link to='/dashboard'>Dashboard</Link></li>
-            <li><Link to='/all-sessions'>All Sessions</Link></li>
-            <li><Link to='/add-session'>Add a Session</Link></li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/sign-up'>Sign Up</Link></li>
-        </ul>
+            <ul>
+                <li className='top-li'><img src={hamburger} className='hamburger' onClick={(e) => toggleNavExpand(e)} /></li>
+                <section className='nav-links'>
+                    <li><Link to='/'>WinRate</Link></li>
+                    <li><Link to='/dashboard'>Dashboard</Link></li>
+                    <li><Link to='/all-sessions'>All Sessions</Link></li>
+                    <li><Link to='/add-session'>Add a Session</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                    <li><Link to='/sign-up'>Sign Up</Link></li>
+                </section>
+            </ul>
         </nav>
     )
+}
+
+function toggleNavExpand(e) {
+    e.target.parentElement.parentElement.parentElement.classList.toggle('nav-expanded');
 }
