5a37ac223b1dcb4d7e761bdb3c48522d55ad53f5
Matthew Long <MattLong87@gmail.com>
2017-06-07 13:21:39 -0400

    styled session info

--- src/components/Session.js
+++ src/components/Session.js
@@ -4,18 +4,22 @@ import {connect} from 'react-redux';
 import '../css/session.css'
 
 export function Session(props) {
-    const players = props.players.map(player => {
-        return `${player} `;
+    const players = props.players.map((player, i) => {
+        if (i === props.players.length - 1){
+            return `${player}`;
+        }
+        return `${player}, `;
     })
     return (
         <div className='session'>
             <section className='title' onClick={(e)=>toggleExpand(e)}>{props.title}</section>
             <section className='session-info'>
-                <section className='players'>Players: {players}</section>
-                <section className='winner'>Winner: {props.winner}</section>
-                <section className='date'>{props.date}</section>
-                <span className='delete-session' onClick={(e)=>{
-                    props.dispatch(actions.deleteSession(props.sessionId, props.token))}}>Delete Session</span>
+                <section className='players'><span className='label'>Players:</span> {players}</section>
+                <section className='winner'><span className='label'>Winner:</span> {props.winner}</section>
+                <section className='date'><span className='label'>Date:</span> {props.date}</section>
+                <span className='delete-session' onClick={(e)=>{props.dispatch(actions.deleteSession(props.sessionId, props.token))}}>
+                    Delete Session
+                </span>
             </section>
         </div>
     )
