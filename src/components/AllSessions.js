e7d878b2607e4893ce24f653c69a1eda93682a8d
Matthew Long <MattLong87@gmail.com>
2017-06-07 12:58:45 -0400

    more styling

--- src/components/AllSessions.js
+++ src/components/AllSessions.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Session from './Session';
+import '../css/allsessions.css'
 
 export function AllSessions(props) {
     return (
-        <div>
+        <div className='all-sessions'>
+            <h1>All Sessions</h1>
             {props.user.sessions.map(function (session, i) {
                 return (
                     <Session key={i} title={session.game} players={session.players} winner={session.winner} date={session.date} sessionId={session._id} token={props.token}/>
