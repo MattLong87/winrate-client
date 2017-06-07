cfba2524cf87613bd3d2448e85d16f9e33f60d1b
Matthew Long <MattLong87@gmail.com>
2017-06-07 11:20:44 -0400

    styling add-session form

--- src/components/AddSession.js
+++ src/components/AddSession.js
@@ -35,22 +35,26 @@ export class AddSession extends React.Component {
 
     render() {
         let playerCheckboxes = this.state.allPlayers.map((player, i) => {
-            return (<div key={i}><input type="checkbox" name="player" value={player} /> {player}</div>)
+            return (<div key={i}><input type="checkbox" name="player" value={player} /><span className="player-checkbox">{player}</span></div>)
         })
         let winnerOptions = this.state.allPlayers.map((player, j) => {
             return (<option key={j}>{player}</option>);
         })
         return (
             <div className='add-session'>
-                <h1>Add a Session</h1>
                 <form id="add-session-form" onSubmit={(e) => this.addSession(e)}>
+                    <h1>Add a Session</h1>
                     <label htmlFor="gameName">Game:</label>
                     <input id="gameName" type="text" name="gameName" placeholder="Game Name" />
                     <div className="players-input">
                         <section id="players-input">
                             <label htmlFor="players">Players:</label>
-                            {playerCheckboxes}
-                            <input id='add-player' onKeyDown={(e) => { this.addPlayer(e) }} type='text' name='add-player' placeholder='Add a player' />
+                            <div className="checkbox-container">
+                                {playerCheckboxes}
+                            </div>
+                            <div className='add-player-container'>
+                                <input id='add-player' onKeyDown={(e) => { this.addPlayer(e) }} type='text' name='add-player' placeholder='Add a player' />
+                            </div>
                         </section>
                     </div>
                     <div className="winner-selection">
