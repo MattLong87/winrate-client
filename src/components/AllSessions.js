bd13711601760837a340c3db6c1a41d5525d7592
Matthew Long <MattLong87@gmail.com>
2017-06-09 15:03:03 -0400

    add-session button on all-sessions page

--- src/components/AllSessions.js
+++ src/components/AllSessions.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Session from './Session';
+import { Link } from 'react-router-dom';
 import '../css/allsessions.css'
 
 export function AllSessions(props) {
     return (
         <div className='all-sessions'>
             <h1>All Sessions</h1>
+            <div className='add-session-link'>
+                <Link to='/add-session'><h4>+ Add a session</h4></Link>
+            </div>
             {props.user.sessions.map(function (session, i) {
                 return (
-                    <Session key={i} title={session.game} players={session.players} winner={session.winner} date={session.date} sessionId={session._id} token={props.token}/>
+                    <Session key={i} title={session.game} players={session.players} winner={session.winner} date={session.date} sessionId={session._id} token={props.token} />
                 )
             })}
         </div>
