3e7ad741abfa597c39c3b217b300609ab226c718
Matthew Long <MattLong87@gmail.com>
2017-08-26 11:39:01 -0400

    fixed date format in recent sessions

--- src/components/RecentSessions.js
+++ src/components/RecentSessions.js
@@ -6,7 +6,7 @@ export default function RecentSessions(props) {
     return (
         <div>
             {props.sessions.map(function (session, i) {
-                let date = new Date(session.date.parseInt());
+                let date = new Date(parseInt(session.date));
                 let formattedDate = (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
                 return (
                     <Session key={i} title={session.game} players={session.players} winner={session.winner} date={formattedDate} sessionId={session._id} token={props.token}/>
